900110c6773efd7aba6c9d2b47d9272e0b7c66c0
Aeon-Avinash <aeon.b.avinash@gmail.com>
2019-11-30 20:35:49 +0530

    fix server to provide client build

---
+++ client/build/precache-manifest.07fad476a8914114ce9d36ad391a8252.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10277ae886e3a98423b706e709e2e8a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b45d4ca727e8922a0f02",
+    "url": "/static/css/2.7f83cc5c.chunk.css"
+  },
+  {
+    "revision": "0471061c2475df5b7a3e",
+    "url": "/static/css/6.7d95f8cf.chunk.css"
+  },
+  {
+    "revision": "d207c28c7068c8b00de0",
+    "url": "/static/css/8.3784bd53.chunk.css"
+  },
+  {
+    "revision": "4a4f19444d9163f5a4fa",
+    "url": "/static/css/9.3784bd53.chunk.css"
+  },
+  {
+    "revision": "f200a3a48e3acd5e6b74",
+    "url": "/static/css/main.32f6c586.chunk.css"
+  },
+  {
+    "revision": "81f6f4e0e896c08ef238",
+    "url": "/static/js/0.9c7a6114.chunk.js"
+  },
+  {
+    "revision": "e7d1ad0ab317fb8a92aa",
+    "url": "/static/js/1.c8387383.chunk.js"
+  },
+  {
+    "revision": "3be5b4aa8322e36c9efd",
+    "url": "/static/js/10.6ff87931.chunk.js"
+  },
+  {
+    "revision": "7e83a0889469fc8d609a",
+    "url": "/static/js/11.64fb14c2.chunk.js"
+  },
+  {
+    "revision": "b45d4ca727e8922a0f02",
+    "url": "/static/js/2.b43b3611.chunk.js"
+  },
+  {
+    "revision": "4603ea273ed76115309a",
+    "url": "/static/js/3.b9c1dd15.chunk.js"
+  },
+  {
+    "revision": "0471061c2475df5b7a3e",
+    "url": "/static/js/6.13992f86.chunk.js"
+  },
+  {
+    "revision": "6a3c457428f56476f23d",
+    "url": "/static/js/7.845a7cc1.chunk.js"
+  },
+  {
+    "revision": "d207c28c7068c8b00de0",
+    "url": "/static/js/8.ee15c2cb.chunk.js"
+  },
+  {
+    "revision": "4a4f19444d9163f5a4fa",
+    "url": "/static/js/9.c0e8c7da.chunk.js"
+  },
+  {
+    "revision": "f200a3a48e3acd5e6b74",
+    "url": "/static/js/main.f45631e5.chunk.js"
+  },
+  {
+    "revision": "6919f95be6da076337fc",
+    "url": "/static/js/runtime-main.94218978.js"
+  },
+  {
+    "revision": "736ba2d18ddca63565879a35dbd02d87",
+    "url": "/static/media/arugula-crust-food-208537.736ba2d1.jpg"
+  }
+]);
