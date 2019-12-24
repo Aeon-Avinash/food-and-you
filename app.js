d1c2bd70abbab1d6b0e55aa3624164ee248f6293
Aeon-Avinash <aeon.b.avinash@gmail.com>
2019-12-24 07:52:03 +0530

    fix serving static files

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ console.log(process.env.NODE_ENV);
 // if (process.env.NODE_ENV !== "development") {
 // console.log("In production mode!");
 // app.use(express.static("client/build"));
-app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static(path.join(__dirname, "/client/build")));
 // // }
 
 app.use(bodyParser.json());
